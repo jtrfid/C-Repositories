a25bf45d11c7656b09134b7e014514b8f2dbfcf3
jtrfid <jtrfid@qq.com>
2016-04-22 22:20:49 +0800

    运动状态，开关门失效

--- Garage_mfc/Elevator_dialog/Elevator_dialogDlg.cpp
+++ Garage_mfc/Elevator_dialog/Elevator_dialogDlg.cpp
@@ -463,6 +463,14 @@ void CElevator_dialogDlg::OnDoubleclickedBtnnum1()
 // 设置电梯内开关门按钮状态，如果当前Light is Off, 置为on;否则保持现状。
 void CElevator_dialogDlg::OnBnClickedBtnOpenCloseDoor()
 {
+	// 安全设置，只有Idle状态，才能开关门
+	if(m_state == MovingUp || m_state == MovingDown) {
+		printf("运动状态，开关门失效!!!\n");
+		// 恢复原值
+		m_Open.setLight(GetOpenDoorLight());
+		m_Close.setLight(GetCloseDoorLight());
+		return;
+	}
 	SetOpenDoorLight(m_Open.getLight());
 	SetCloseDoorLight(m_Close.getLight());
 }
