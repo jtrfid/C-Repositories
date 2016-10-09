dc4005ff44bb455e2db70ee108640e72737cb047
jtrfid <jtrfid@qq.com>
2016-10-10 00:30:58 +0800

    ch1学习指导

---
+++ 2016/C语言/学习指导dev工程/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void ch1(); 
+void ch2();
+void ch3();
+void ch4(); 
+void ch5();
+void ch6();
+void ch7();
+void ch8(); 
+void ch9();
+ 
+int main(int argc, char *argv[])
+{
+  printf("C语言程序设计学习指导汇总:\n"); 
+  
+  ch1();  
+  //ch2(); 
+  //ch3();
+  //ch4(); 
+  //ch5();
+  //ch6();
+  //ch7();
+  //ch8(); 
+  //ch9();
+  system("PAUSE");	 // 同system("pause");
+  return 0;
+}
